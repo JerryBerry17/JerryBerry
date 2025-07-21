f377d19a127211eebd4158981a8beee31f47b6d9
JerryBerry17 <jermainetkd@gmail.com>
2025-07-21 05:56:23 +0000

    work done for day1

---
+++ Part1/ex03.py
@@ -0,0 +1,17 @@
+# Original text
+string1 = 'Daddy'
+
+#convert to lowercase
+string_lower = "daddy"
+
+#convert to uppercase 
+string_upper = "DADDY" 
+
+#remove spaces
+string_nospace = "Daddy"
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
